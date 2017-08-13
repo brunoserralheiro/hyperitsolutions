532b4600f53e063d1c4c8b187ef71ac377d3bf48
brunoserralheiro <bruno@serralheiro.eu>
2017-08-13 21:45:20 +0100

    FIX repository component scan.

---
+++ src/main/java/hyperitsolutions/ship/ShipApplication.java
@@ -0,0 +1,141 @@
+package hyperitsolutions.ship;
+
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.apache.commons.dbcp.BasicDataSource;
+import org.hibernate.jpa.AvailableSettings;
+import org.hibernate.jpa.HibernatePersistenceProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import hyperitsolutions.ship.model.OrderRepository;
+import hyperitsolutions.ship.model.entity.Order;
+
+@PropertySource(value = { "classpath:jdbc.properties" })
+@SpringBootApplication
+//@Configuration  // REMEMBER TO NEVER USE THIS ANNOTATION ALONG WITH THE BELOW ONES!!!
+@EnableJpaRepositories(basePackages = "hyperitsolutions.ship.model" )
+@EnableTransactionManagement
+@ComponentScan(basePackages= "hyperitsolutions.ship" )
+public class ShipApplication {
+
+	private static final String PROPERTY_NAME_HIBERNATE_DIALECT = "hibernate.dialect";
+	private static final String PROPERTY_NAME_HIBERNATE_MAX_FETCH_DEPTH = "hibernate.max_fetch_depth";
+	private static final String PROPERTY_NAME_HIBERNATE_JDBC_FETCH_SIZE = "hibernate.jdbc.fetch_size";
+	private static final String PROPERTY_NAME_HIBERNATE_JDBC_BATCH_SIZE = "hibernate.jdbc.batch_size";
+	private static final String PROPERTY_NAME_HIBERNATE_SHOW_SQL = "hibernate.show_sql";
+	private static final String ENTITYMANAGER_PACKAGES_TO_SCAN = "hyperitsolutions.ship.model.entity";
+	private static final String HIBERNATE_HBM2DDL_AUTO = "hibernate.hbm2ddl.auto";
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ShipApplication.class, args);
+		
+	}
+
+	@Autowired
+	private Environment env;
+	
+//	@Autowired
+//	private OrderRepository orderRepository;
+
+	@Bean(name = "lab1")
+	public DataSource dataSource() {
+		BasicDataSource dataSource = new BasicDataSource();
+		dataSource.setDriverClassName(env.getProperty("jdbc.driverClassName"));
+		dataSource.setUrl(env.getProperty("jdbc.url"));
+		dataSource.setUsername(env.getProperty("jdbc.username"));
+		dataSource.setPassword(env.getProperty("jdbc.password"));
+		return dataSource;
+	}
+
+	@Bean(name="transactionManager")
+	public JpaTransactionManager jpaTransactionManager() {
+		JpaTransactionManager transactionManager = new JpaTransactionManager();
+		transactionManager.setEntityManagerFactory(entityManagerFactoryBean().getObject());
+		return transactionManager;
+	}
+
+	private HibernateJpaVendorAdapter vendorAdaptor() {
+		HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+		vendorAdapter.setShowSql(true);
+		return vendorAdapter;
+	}
+
+	@Bean(name="entityManagerFactory")
+	public LocalContainerEntityManagerFactoryBean entityManagerFactoryBean() {
+
+		LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
+		entityManagerFactoryBean.setPersistenceUnitName("lab1");
+		entityManagerFactoryBean.setJpaVendorAdapter(vendorAdaptor());
+		entityManagerFactoryBean.setDataSource(dataSource());
+		entityManagerFactoryBean.setPersistenceProviderClass(HibernatePersistenceProvider.class);
+		entityManagerFactoryBean.setPackagesToScan(ENTITYMANAGER_PACKAGES_TO_SCAN);
+		entityManagerFactoryBean.setJpaProperties(jpaHibernateProperties());
+
+		return entityManagerFactoryBean;
+	}
+
+	private Properties jpaHibernateProperties() {
+
+		Properties properties = new Properties();
+
+		properties.put(PROPERTY_NAME_HIBERNATE_MAX_FETCH_DEPTH,
+				env.getProperty(PROPERTY_NAME_HIBERNATE_MAX_FETCH_DEPTH));
+		properties.put(PROPERTY_NAME_HIBERNATE_JDBC_FETCH_SIZE,
+				env.getProperty(PROPERTY_NAME_HIBERNATE_JDBC_FETCH_SIZE));
+		properties.put(PROPERTY_NAME_HIBERNATE_JDBC_BATCH_SIZE,
+				env.getProperty(PROPERTY_NAME_HIBERNATE_JDBC_BATCH_SIZE));
+		properties.put(PROPERTY_NAME_HIBERNATE_SHOW_SQL, env.getProperty(PROPERTY_NAME_HIBERNATE_SHOW_SQL));
+		properties.put(PROPERTY_NAME_HIBERNATE_DIALECT, env.getProperty(PROPERTY_NAME_HIBERNATE_DIALECT));
+		properties.put(HIBERNATE_HBM2DDL_AUTO, env.getProperty(HIBERNATE_HBM2DDL_AUTO));
+
+		properties.put(AvailableSettings.SCHEMA_GEN_DATABASE_ACTION, "none");
+		return properties;
+	}
+	
+	
+	
+	
+	@Bean
+    public CommandLineRunner run(OrderRepository orderRepository) {
+        return args -> {
+
+        	if (null != orderRepository) {
+				System.out.println("repository NOT null!");
+				Order order2 = new Order();
+				order2.setName("ordertwo");
+				order2.setId(1L);
+				order2 = orderRepository.save(order2);
+			}else {
+				System.out.println("repository is null");
+			}
+            
+
+        };
+    }
+	
+	static class InnerShipApp{
+	
+		
+		
+		static void run(OrderRepository orderRepository) {
+			
+		}
+	}
+
+}
